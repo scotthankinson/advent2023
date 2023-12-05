f4deb6c6330c2935c07c32259b8a9e779992bf4d
Scott Hankinson <scott.hankinson@slalom.com>
2023-12-05 01:33:52 -0500

    day 5

--- src/index.ts
+++ src/index.ts
@@ -2,82 +2,40 @@
 import * as fs from 'fs';
 
 const start = (): void => {
-    // console.log(solve_pt1());
-    console.log(solve_pt2());
+    console.log(solve_pt1());
+    // console.log(solve_pt2());
 };
 
 
-/*
+
 const solve_pt1 = () => {
     try {
-        let data = fs.readFileSync('src/input.dec4.txt', 'utf8');
+        let data = fs.readFileSync('src/input.dec6.txt', 'utf8');
         const lines = data.split('\n');
-        let cards = [];
-        let total = 0;
-        for(let line of lines) {
-            let parts = line.split(': ')[1].split(' | ');
-            let card = {'winners': parts[0].split(' ').filter(o => o.trim().length > 0), 'candidates': parts[1].split(' ').filter(o => o.trim().length > 0), 'matches': [], 'value': 0}
-            for(let winner of card.winners){
-                if (card.candidates.indexOf(winner) >= 0) card.matches.push(winner);
-            }
-            for(let i = 0; i < card.matches.length; i++){
-                if (card.value === 0) card.value = 1;
-                else card.value = card.value * 2;
-            }
-            total = total + card.value;
-            console.log(card);
-            cards.push(card);
-        }
-        return total;
-    } catch (e) {
-        console.log('Error:', e.stack);
-    }
-    return -1;
-}*/
+        console.log(lines);
 
-
-
-const solve_pt2 = () => {
-    try {
-        let data = fs.readFileSync('src/input.dec4.txt', 'utf8');
-        const lines = data.split('\n');
-        let cards = [];
-        for(let line of lines) {
-            let parts = line.split(': ')[1].split(' | ');
-            let card = {
-                'card': line.split(': ')[0], 
-                'winners': parts[0].split(' ').filter(o => o.trim().length > 0), 
-                'candidates': parts[1].split(' ').filter(o => o.trim().length > 0), 
-                'matches': [], 
-                'copies': 1 
-            }
-            for(let winner of card.winners){
-                if (card.candidates.indexOf(winner) >= 0) card.matches.push(winner);
-            }
-            cards.push(card);
-        }
-
-        let checkedCards = [];
-        while (cards.length > 0) {
-            let currentCard = cards.shift();
-            checkedCards.push(currentCard);
-            for(let i = 0; i < currentCard.copies; i++){
-                for(let j = 0; j < currentCard.matches.length; j++){
-                    cards[j].copies += 1;
-                }
-            }
-        }
-        let totalCards = 0;
-        for(let card of checkedCards) {
-            totalCards += card.copies;
-        }
-        console.log(cards);
-        return totalCards;
+        return 0;
     } catch (e) {
         console.log('Error:', e.stack);
     }
     return -1;
 }
+
+
+/*
+const solve_pt2 = () => {
+    try {
+        let data = fs.readFileSync('src/input.dec6.txt', 'utf8');
+        const lines = data.split('\n');
+        console.log(lines);
+
+        return 0;
+    } catch (e) {
+        console.log('Error:', e.stack);
+    }
+    return -1;
+}
+*/
 
 
 
---
+++ src/solved/index.dec5.ts
@@ -0,0 +1,222 @@
+"use strict";
+import * as fs from 'fs';
+
+const start = (): void => {
+    // console.log(solve_pt1());
+    console.log(solve_pt2());
+};
+
+
+/*
+const solve_pt1 = () => {
+    try {
+        let data = fs.readFileSync('src/input.dec5.txt', 'utf8');
+        const lines = data.split('\n');
+        let mode = 'seed';
+        let seeds = [];
+        let maps = {};
+        for(let line of lines){
+            if (line.trim().length === 0) continue;
+            if (line.startsWith('seeds:')){
+                seeds = line.replace('seeds: ', '').split(' ');
+            } else if (line.indexOf('-to-') >= 0) {
+                mode = line.split(' ')[0];
+            } else {
+                if (!maps[mode]) maps[mode] = [];
+                let parts = line.split(' ');
+                maps[mode].push({
+                    'sourceFrom': parseInt(parts[1]),
+                    'sourceTo': parseInt(parts[1]) + parseInt(parts[2]) - 1,
+                    'offset': parseInt(parts[0]) - parseInt(parts[1])
+                });
+            }
+        }
+        
+        let minSeed = 999999999;
+        console.log(seeds);
+        console.log(maps);
+        for(let i = 0; i < seeds.length; i++){
+            console.log("----------");
+            let seed = parseInt(seeds[i]);
+            console.log("Seed " + seed);
+            let soil = seed;
+            for(let j = 0; j < maps['seed-to-soil'].length; j++){
+                if (seed >= maps['seed-to-soil'][j].sourceFrom && seed <= maps['seed-to-soil'][j].sourceTo){
+                    soil = seed + maps['seed-to-soil'][j].offset;
+                    break;
+                }
+            }
+            console.log("Soil " + soil);
+            let fertilizer = soil;
+            for(let j = 0; j < maps['soil-to-fertilizer'].length; j++){
+                if (fertilizer >= maps['soil-to-fertilizer'][j].sourceFrom && fertilizer <= maps['soil-to-fertilizer'][j].sourceTo){
+                    fertilizer = soil + maps['soil-to-fertilizer'][j].offset;
+                    break;
+                }
+            }
+            console.log("Fertilizer " + fertilizer);
+            let water = fertilizer;
+            for(let j = 0; j < maps['fertilizer-to-water'].length; j++){
+                if (water >= maps['fertilizer-to-water'][j].sourceFrom && water <= maps['fertilizer-to-water'][j].sourceTo){
+                    water = fertilizer + maps['fertilizer-to-water'][j].offset;
+                    break;
+                }
+            }
+            console.log("Water " + water);
+            let light = water;
+            for(let j = 0; j < maps['water-to-light'].length; j++){
+                if (light >= maps['water-to-light'][j].sourceFrom && light <= maps['water-to-light'][j].sourceTo){
+                    light = water + maps['water-to-light'][j].offset;
+                    break;
+                }
+            }
+            console.log("Light " + light);
+            let temperature = light;
+            for(let j = 0; j < maps['light-to-temperature'].length; j++){
+                if (temperature >= maps['light-to-temperature'][j].sourceFrom && temperature <= maps['light-to-temperature'][j].sourceTo){
+                    temperature = light + maps['light-to-temperature'][j].offset;
+                    break;
+                }
+            }
+            console.log("Temperature " + temperature);
+            let humidity = temperature;
+            for(let j = 0; j < maps['temperature-to-humidity'].length; j++){
+                if (humidity >= maps['temperature-to-humidity'][j].sourceFrom && humidity <= maps['temperature-to-humidity'][j].sourceTo){
+                    humidity = temperature + maps['temperature-to-humidity'][j].offset;
+                    break;
+                }
+            }
+            console.log("Humidity " + humidity);
+            let location = humidity;
+            for(let j = 0; j < maps['humidity-to-location'].length; j++){
+                if (location >= maps['humidity-to-location'][j].sourceFrom && location <= maps['humidity-to-location'][j].sourceTo){
+                    location = humidity + maps['humidity-to-location'][j].offset;
+                    break;
+                }
+            }
+            console.log("Location " + location);
+            if (location < minSeed) minSeed = location;
+            console.log("Seed " + seeds[i] + " to " + location);
+        }   
+
+        return minSeed;
+    } catch (e) {
+        console.log('Error:', e.stack);
+    }
+    return -1;
+}
+*/
+
+const solve_pt2 = () => {
+    try {
+        let data = fs.readFileSync('src/input.dec5.txt', 'utf8');
+        const lines = data.split('\n');
+        let mode = 'seed';
+        let seeds = [];
+        let maps = {};
+        for(let line of lines){
+            if (line.trim().length === 0) continue;
+            if (line.startsWith('seeds:')){
+                seeds = line.replace('seeds: ', '').split(' ').map(o => parseInt(o));
+            } else if (line.indexOf('-to-') >= 0) {
+                mode = line.split(' ')[0];
+            } else {
+                if (!maps[mode]) maps[mode] = [];
+                let parts = line.split(' ');
+                maps[mode].push({
+                    'sourceFrom': parseInt(parts[1]),
+                    'sourceTo': parseInt(parts[1]) + parseInt(parts[2]) - 1,
+                    'offset': parseInt(parts[0]) - parseInt(parts[1])
+                });
+            }
+        }
+
+        let foundMinSeed = 9999999999999;
+        while (seeds.length > 0){
+            console.log(seeds.length + " left to process");
+            let from = seeds.shift();
+            let range = seeds.shift();
+            console.log("From: " + from + " for " + range + " entries");
+            for(let i = 0; i < range; i++){
+                let checkSeed = minSeed(from + i, maps);
+                if (checkSeed < foundMinSeed) foundMinSeed = checkSeed;
+            }
+        }
+        return foundMinSeed;
+    } catch (e) {
+        console.log('Error:', e.stack);
+    }
+    return -1;
+}
+
+const minSeed = (seed, maps) => {
+    let minSeed = 9999999999999;
+    //console.log("----------");
+    //console.log("Seed " + seed);
+    let soil = seed;
+    for(let j = 0; j < maps['seed-to-soil'].length; j++){
+        if (seed >= maps['seed-to-soil'][j].sourceFrom && seed <= maps['seed-to-soil'][j].sourceTo){
+            soil = seed + maps['seed-to-soil'][j].offset;
+            break;
+        }
+    }
+    //console.log("Soil " + soil);
+    let fertilizer = soil;
+    for(let j = 0; j < maps['soil-to-fertilizer'].length; j++){
+        if (fertilizer >= maps['soil-to-fertilizer'][j].sourceFrom && fertilizer <= maps['soil-to-fertilizer'][j].sourceTo){
+            fertilizer = soil + maps['soil-to-fertilizer'][j].offset;
+            break;
+        }
+    }
+    //console.log("Fertilizer " + fertilizer);
+    let water = fertilizer;
+    for(let j = 0; j < maps['fertilizer-to-water'].length; j++){
+        if (water >= maps['fertilizer-to-water'][j].sourceFrom && water <= maps['fertilizer-to-water'][j].sourceTo){
+            water = fertilizer + maps['fertilizer-to-water'][j].offset;
+            break;
+        }
+    }
+    //console.log("Water " + water);
+    let light = water;
+    for(let j = 0; j < maps['water-to-light'].length; j++){
+        if (light >= maps['water-to-light'][j].sourceFrom && light <= maps['water-to-light'][j].sourceTo){
+            light = water + maps['water-to-light'][j].offset;
+            break;
+        }
+    }
+    //console.log("Light " + light);
+    let temperature = light;
+    for(let j = 0; j < maps['light-to-temperature'].length; j++){
+        if (temperature >= maps['light-to-temperature'][j].sourceFrom && temperature <= maps['light-to-temperature'][j].sourceTo){
+            temperature = light + maps['light-to-temperature'][j].offset;
+            break;
+        }
+    }
+    //console.log("Temperature " + temperature);
+    let humidity = temperature;
+    for(let j = 0; j < maps['temperature-to-humidity'].length; j++){
+        if (humidity >= maps['temperature-to-humidity'][j].sourceFrom && humidity <= maps['temperature-to-humidity'][j].sourceTo){
+            humidity = temperature + maps['temperature-to-humidity'][j].offset;
+            break;
+        }
+    }
+    //console.log("Humidity " + humidity);
+    let location = humidity;
+    for(let j = 0; j < maps['humidity-to-location'].length; j++){
+        if (location >= maps['humidity-to-location'][j].sourceFrom && location <= maps['humidity-to-location'][j].sourceTo){
+            location = humidity + maps['humidity-to-location'][j].offset;
+            break;
+        }
+    }
+    //console.log("Location " + location);
+    if (location < minSeed) minSeed = location;
+
+    return minSeed;
+}
+
+
+
+start();
+
+
+export default start;
